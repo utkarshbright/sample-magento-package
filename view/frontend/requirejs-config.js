e7d1f5b00faf16afb0e35fff79b29aee6b60a524
utkarshbright <utkarsh@surebright.com>
2024-12-06 12:20:01 +0000

    Initial

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        sbsimsdk: 'https://nice-glowworm-blessed.ngrok-free.app/dist/sbsimsdk' // Path without .js extension
+    },
+    shim: {
+        sbsimsdk: {
+            deps: [] // Add dependencies if required, e.g., ['jquery']
+        }
+    }
+};
